33b510b42b1d7087c27938e1f9d4861cf82c47fe
DanielL <rra.daniel@gmail.com>
2022-06-29 13:57:11 -0400

    clase 232

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SidemenuComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    SidemenuComponent
   ]
 })
 export class SharedModule { }
